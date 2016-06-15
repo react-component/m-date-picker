4791578f7b6bd32891ea7b42d041710fb7a015d7
yiminghe <yiminghe@gmail.com>
2016-06-15 22:08:26 +0800

    clean

--- examples/popup.d.ts
+++
@@ -1,3 +0,0 @@
-import 'rmc-picker/assets/index.css';
-import 'rmc-date-picker/assets/index.less';
-import 'rmc-picker/assets/popup.css';
--- examples/time.d.ts
+++
@@ -1,2 +0,0 @@
-import 'rmc-picker/assets/index.css';
-import 'rmc-date-picker/assets/index.less';
