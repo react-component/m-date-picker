cb0da51217ef6752902a8465fdab8f483bde15e9
yiminghe <yiminghe@gmail.com>
2016-09-22 21:35:17 +0800

    fix android picker

--- src/DatePicker.tsx
+++ src/DatePicker.tsx
@@ -1,4 +1,4 @@
-import {View, StyleSheet} from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import * as React from 'react';
 import Picker from 'rmc-picker/lib/Picker';
 import DatePickerProps from './DatePickerProps';
@@ -8,6 +8,9 @@ import FlexAlignType = __React.FlexAlignType;
 type FlexDirection = 'row' | 'column';
 
 const styles = StyleSheet.create({
+  smallPickerItem: {
+    fontSize: 20,
+  },
   item: {
     flex: 1,
   },
@@ -21,13 +24,14 @@ const styles = StyleSheet.create({
 
 const DatePicker = React.createClass<DatePickerProps, any>({
   mixins: [DatePickerMixin],
-  
-  render() {
-    const {value, dataSource} = this.getValueDataSource();
 
+  render() {
+    const { value, dataSource } = this.getValueDataSource();
+    const { mode } = this.props;
     const inner = dataSource.map((items, i) => {
       return (<View style={styles.item} key={i}>
         <Picker
+          itemStyle={mode === 'datetime' ? styles.smallPickerItem : undefined}
           pure={false}
           selectedValue={value[i]}
           onValueChange={(v) => {this.onValueChange(i, v);}}
