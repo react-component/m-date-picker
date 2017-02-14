99f796da2a1125eb4158214cb4e7cbf69126132b
yiminghe <yiminghe@gmail.com>
2017-02-14 17:28:58 +0800

    update demo

--- index.ios.js
+++ index.ios.js
@@ -4,8 +4,8 @@ import getList from 'react-native-index-page';
 
 getList({
   demos: [
-    require('./examples/react-native/picker'),
-    require('./examples/react-native/popup'),
+    require('./_ts2js/examples/react-native/picker'),
+    require('./_ts2js/examples/react-native/popup'),
   ],
   title: require('./package.json').name,
 });
